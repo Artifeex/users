744d31ed74246f11ec721d8113a00051f2dceb92
Artifeex <dreamerandrew17@gmail.com>
2026-05-11 21:59:29 +0300

    add prometheus и grafana

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("org.liquibase:liquibase-core")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     compileOnly("org.projectlombok:lombok")
--- src/main/java/ru/sandr/users/security/config/SecurityConfig.java
+++ src/main/java/ru/sandr/users/security/config/SecurityConfig.java
@@ -62,7 +62,9 @@ public class SecurityConfig {
                                                "/swagger-ui.html",
                                                "/swagger-ui/**",
                                                "/v3/api-docs",
-                                               "/v3/api-docs/**"
+                                               "/v3/api-docs/**",
+                                               "/actuator/health",
+                                               "/actuator/prometheus"
                                        )
                                        .permitAll()
                                        .requestMatchers("/auth/**").permitAll()
